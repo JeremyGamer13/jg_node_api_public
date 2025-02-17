f1d9a9e3ece98e124702b3b87ee29283f4479596
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-02-16 18:00:43 -0700

    yoink from gextapi

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 8080;
+
+// TODO: Automatically load endpoints.
+const endpointTTS = require("./src/api/tts");
+
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
+app.use(bodyParser.urlencoded({
+    limit: "10kb",
+    extended: false
+}));
+app.use(bodyParser.json({ limit: "10kb" }));
+
+app.get('/', async function (_, res) {
+    res.send("ermm.. what the sigma...");
+});
+
+app.get('/tts', endpointTTS);
+
+app.listen(port, () => console.log('Started server on port ' + port));
